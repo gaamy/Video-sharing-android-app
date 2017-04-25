47a7a36b6cb276b0812be5def18d7c0c88b03928
Gabriel Amyot <gabriel.amyot@gmail.com>
2017-04-25 14:47:05 -0400

    Adding simple POJO classes for database handling

--- app/src/main/java/com/polymt/inf8405/tp3/database/DatabaseManager.java
+++ app/src/main/java/com/polymt/inf8405/tp3/database/DatabaseManager.java
@@ -1,16 +1,50 @@
 package com.polymt.inf8405.tp3.database;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.polymt.inf8405.tp3.database.pojo.DatabaseUser;
+
 /**
  * Created by gamyot on 2017-04-12.
  */
 
 public class DatabaseManager {
 
-    void pushUser(){}
+
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseUsers;
+    private DatabaseReference mDatabaseVideos;
+
+
+    public void initialize(){
+        //initialise database references
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseUsers = mFirebaseDatabase.getReference().child("users");
+        mDatabaseVideos = mFirebaseDatabase.getReference().child("videos");
+        // attach listeners
+    }
+
+    public void createNewUSer(String name, String email){
+        DatabaseUser newUser = new DatabaseUser(name,email);
+        DatabaseReference newUserReference = mDatabaseUsers.push();
+        newUser.setUserId(newUserReference.getKey());
+        newUserReference.setValue(newUser);
+    }
+
+
+    void testMethod(){
+
+    }
+
+
+
     void updateUser(){}
 
     void updateVideo(){}
     void pushVideo(){}
+
+
 
 
 
